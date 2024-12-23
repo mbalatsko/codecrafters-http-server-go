8e35ac906c215c920a535e4a2370f6df428becbb
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2024-12-23 14:25:01 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,20 +2,62 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"net/http"
 	"os"
 )
 
+const httpVersion = "HTTP/1.1"
+const eol = "\r\n"
+
+type HttpStatus struct {
+	Code int
+}
+
+func (status *HttpStatus) String() string {
+	return fmt.Sprintf("%d %s", status.Code, http.StatusText(status.Code))
+}
+
+type HttpResponse struct {
+	Status  HttpStatus
+	Headers map[string]string
+	Body    string
+}
+
+func makeHttpResponse(status HttpStatus) *HttpResponse {
+	return &HttpResponse{
+		Status:  status,
+		Headers: make(map[string]string),
+		Body:    "",
+	}
+}
+
+func (response *HttpResponse) String() string {
+	return fmt.Sprintf("%s %s", httpVersion, &response.Status) + eol + eol
+}
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	addr := "0.0.0.0:4221"
+	l, err := net.Listen("tcp", addr)
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		log.Fatal("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+	log.Printf("Started TCP server on %s\n", addr)
+
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		log.Fatal("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	log.Printf("Connection received from %s\n", conn.RemoteAddr())
+	defer conn.Close()
+
+	response := makeHttpResponse(HttpStatus{200}).String()
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		log.Fatal("Error writting to connection: ", err.Error())
 		os.Exit(1)
 	}
 }
